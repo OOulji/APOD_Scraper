9dae1de59b1be264f32a8f52d6c102e838491faa
Omar <omaroulj@gmail.com>
2021-12-10 12:31:02 +0100

    Prints title

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Small script that grabs NASA's astronomy pic of the day and saves it in the script folder. Nothing fancy, just learning Beautiful Soup. 
+
+Right now, it grabs today's pic, saves it and prints the title in the terminal.
+
+TODO: I'd like to print the description too. 
--- main.py
+++ main.py
@@ -9,24 +9,28 @@ def get_date():
     return d
 
 def main(date):
+
+    print("Connecting...\n")
+
     data = requests.get("https://apod.nasa.gov/apod/ap"+date+".html")
+
+    print("Data request succesfull...\n")
+
     soup = BeautifulSoup(data.text, 'html.parser')
 
-  
     image = soup.find('img')
+    title = soup.find('title')
 
     if image is not None:
         image_url = image['src']
         img = Image.open(requests.get("https://apod.nasa.gov/apod/"+image_url, stream = True).raw)
         img.save(date+".jpg")
-        print("Image found and saved")
+        print("Image found and saved!\n")
+        print(title.get_text())
         img.show()
     else:
         print("No image found.")
 
-
-    
-    
 
 if __name__ == "__main__":
     day = get_date()
