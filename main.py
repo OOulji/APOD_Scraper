4557c68e5a571ec045054a423bd2502461005635
Omar <omaroulj@gmail.com>
2021-12-12 00:30:59 +0100

    Solves an issue found when the image is not up yet

--- main.py
+++ main.py
@@ -8,11 +8,15 @@ def get_date():
     d=date.today().strftime("%y%m%d")
     return d
 
-def main(date):
+def fetch_image(date):
 
     print("Connecting...\n")
 
     data = requests.get("https://apod.nasa.gov/apod/ap"+date+".html")
+
+    if data.status_code == 404:
+        print("Today's image is not up yet!")
+        return
 
     print("Data request succesfull...\n")
 
@@ -34,4 +38,4 @@ def main(date):
 
 if __name__ == "__main__":
     day = get_date()
-    main(day)
+    fetch_image(day)
